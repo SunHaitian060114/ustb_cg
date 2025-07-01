e6ebe76e011b8e636b1b2b8f5ecde63ed46bfea7
SunHaitian060114 <107476564+SunHaitian060114@users.noreply.github.com>
2025-07-01 09:25:18 +0800

    fizzBuzz.8

--- fizzBuzz.cpp
+++ fizzBuzz.cpp
@@ -3,18 +3,12 @@
 #include <cstring>
 #include <string>
 using namespace std;
-/*string a;
-	if (!(value % 3) && !(value % 5))
-		return "FizzBuzz";
-	if (!(value % 5))
-		return "Buzz";
-	if (!(value % 3))
-		return "Fizz";
-	return to_string(value);
-*/
+
 
 string fizzBuzz(int value)
 {
+	if (!(value % 3) && !(value % 5))
+		return "FizzBuzz";
 	if (!(value % 3))
 		return "Fizz";
 	if (!(value % 5))
