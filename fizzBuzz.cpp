38c2fdc0e5afb6909989475744d2f2b549ed8c73
SunHaitian060114 <107476564+SunHaitian060114@users.noreply.github.com>
2025-07-01 09:05:03 +0800

    fizzBuzz.1

---
+++ fizzBuzz.cpp
@@ -0,0 +1,21 @@
+#include "fizzBuzz.h"
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+/*string a;
+	if (!(value % 3) && !(value % 5))
+		return "FizzBuzz";
+	if (!(value % 5))
+		return "Buzz";
+	if (!(value % 3))
+		return "Fizz";
+	return to_string(value);
+*/
+
+string fizzBuzz(int value)
+{
+	return to_string(value);
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "fizzbuzz.h"
+using namespace std;
+//#include"gtest/gtest.h"
+
+void test(int input, int output, int aim)
+{
+	//The input corresponds to the intput value in TodoList
+	//The output corresponds to the expected output
+	//The aim corresponds to the serial number in TodoList
+	string result = fizzBuzz(input);
+	if (result == to_string(output))
+	{
+		cout << "Test " << aim << " passed. Expected: " << output << ", got: " << result << endl;
+	}
+	else
+	{
+		cout << "Test " << aim << " failed. Expected: " << output << ", got: " << result << endl;
+	}
+
+}
+
+int main()
+{
+    int value;
+    //cin >> value;
+    //cout << fizzBuzz(value) << endl;
+	test(1, 1, 1);
+	test(2, 2, 1);
+    return 0;
+}
